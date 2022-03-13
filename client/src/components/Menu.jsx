57c1fc8a3ab4a87491051ff03883fc457fb2742f
Rui.Chuang@cibc.com <98039927+rccrc@users.noreply.github.com>
2022-03-13 16:53:45 -0400

    conditional style on active button

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -4,14 +4,20 @@ import { useNavigate } from 'react-router-dom'
 import { MdShoppingCart, MdSettings } from 'react-icons/md'
 import { RiAwardFill } from 'react-icons/ri'
 import { BsStarFill } from 'react-icons/bs'
+import { useState } from 'react'
 
 const Menu = () => {
   // common css styles
   const buttonCommonStyle =
     'flex justify-start items-center text-base py-3 xl:px-4 pl-2 xl:font-semibold font-medium mb-3 rounded-lg cursor-pointer'
-  const buttonIconCommonStyle = 'mr-2 text-2xl'
-  const inactiveStyle = 'text-[#A5A6F6]'
-  const activeStyle = 'bg-[#5D5FEF] text-white'
+
+  const activeBtnStyle = `${buttonCommonStyle} bg-[#5D5FEF] text-white`
+  const activeIconStyle = 'mr-2 text-2xl text-white'
+
+  const inactiveIconStyle = 'mr-2 text-2xl text-[#A5A6F6]'
+  const inactiveBtnStyle = `text-[#A5A6F6] ${buttonCommonStyle}`
+
+  const [activeBtn, setActiveBtn] = useState(true)
 
   // navigation
   const navigate = useNavigate()
@@ -19,19 +25,33 @@ const Menu = () => {
     navigate('/nominations')
   }
 
+  // mouse hover event
+  const mouseEnter = () => {
+    setActiveBtn(false)
+  }
+
+  const mouseLeave = () => {
+    setActiveBtn(true)
+  }
+
   return (
     <div className="xl:px-6 flex flex-col bg-slate-500 bg-opacity-0 pt-20">
-      <button className={`${activeStyle} ${buttonCommonStyle}`}>
-        <RiAwardFill className={`text-white ${buttonIconCommonStyle}`} /> My Awards
+      {/* <button className={`${awardBtnStyle}`}> */}
+      <button className={`${activeBtn ? activeBtnStyle : inactiveBtnStyle}`}>
+        <RiAwardFill className={`${activeBtn ? activeIconStyle : inactiveIconStyle}`} /> My Awards
       </button>
-      <button onClick={toMyNominations} className={`${buttonCommonStyle} ${inactiveStyle}`}>
-        <BsStarFill className={`${buttonIconCommonStyle} ${inactiveStyle} `} /> My Nominations
+      <button
+        onClick={toMyNominations}
+        onMouseEnter={mouseEnter}
+        onMouseLeave={mouseLeave}
+        className={`${!activeBtn ? activeBtnStyle : inactiveBtnStyle}`}>
+        <BsStarFill className={`${!activeBtn ? activeIconStyle : inactiveIconStyle}`} /> My Nominations
       </button>
-      <button className={`${buttonCommonStyle} ${inactiveStyle}`}>
-        <MdShoppingCart className={`${buttonIconCommonStyle} ${inactiveStyle}`} /> Shopping
+      <button className={`${inactiveBtnStyle}`}>
+        <MdShoppingCart className={`${inactiveIconStyle}`} /> Shopping
       </button>
-      <button className={`${buttonCommonStyle} ${inactiveStyle}`}>
-        <MdSettings className={`${buttonIconCommonStyle} ${inactiveStyle}`} /> Setting
+      <button className={`${inactiveBtnStyle}`}>
+        <MdSettings className={`${inactiveIconStyle}`} /> Setting
       </button>
 
     </div>
