97eaa7663ae48c836d4e25dd6cc1f18fd4801287
danielssonn <123456kolokol>
2022-04-08 16:47:44 -0400

    Comments added

--- smart-contracts/contracts/StakingToken.sol
+++ smart-contracts/contracts/StakingToken.sol
@@ -5,6 +5,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
+/**
+ * Initial balance of Staking tokens is be minted for each participant, upon wallet connection
+ * Staking Tokens can be added to wallet using the Import Token feature
+ * Staking Token is a utility token, without economic value
+ */
 contract StakingToken is ERC20, Ownable {
     uint256 constant _initial_supply = 10000 * (10**18);
 
@@ -14,10 +19,16 @@ contract StakingToken is ERC20, Ownable {
         _mint(msg.sender, _initial_supply);
     }
 
+    /**
+     * Owner can increase the inital supply
+     */
     function _testReplenishSupply(uint256 amount) public onlyOwner {
         _mint(msg.sender, amount * (10 * 18));
     }
 
+    /**
+     * Distribute Staking Tokens to users for staking, once only, upon inital wallet connection to contract
+     */
     function mint(address to, uint256 amount) public returns (bool) {
         if (balanceOf(to) == 0 && !tokenHolders[to]) {
             console.log("will mint", balanceOf(to), tokenHolders[to]);
